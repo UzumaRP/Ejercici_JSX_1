848a11b6f25bd1400a6c4c0e401e997dbacc35fc
Unai <unrupe@alumnatflorida.es>
2025-10-23 16:04:34 +0200

    Exercici acabat

--- app/inici.js
+++ app/inici.js
@@ -22,32 +22,50 @@ const Dades = (elementos, estilAAplicar) => {
 }
 
 const Informe = () => {
-
-  <Button icon="format-list-bulleted" mode="contained">
-    Informe
-  </Button>
+  return (
+    <View style = {{margin: 10}}>
+    <Button icon="format-list-bulleted" mode="contained">
+      INFORME
+    </Button>
+    </View>
+  );
 }
+
 
 const Inici = () => {
   //const [estil, setEstil] = useState('florida');
-  const estil = 'florida';
+  const estil = 'upv';
   const isAdmin = true;
   const moduls2Dam = [
-{ nom: 'DIN', professor: 'Manel', hores: 120 },
-{ nom: 'ADA', professor: 'Roberto', hores: 120 },
-{ nom: 'PMDM', professor: 'Paco', hores: 100 },
-{ nom: 'PSP', professor: 'Roberto', hores: 60 },
-{ nom: 'SGE', professor: 'Belén', hores: 100 },
-{ nom: 'Anglés', professor: 'Caterina', hores: 40 },
-{ nom: 'EIE', professor: 'Ana', hores: 60 },
-];
+    { nom: 'DIN', professor: 'Manel', hores: 120 },
+    { nom: 'ADA', professor: 'Roberto', hores: 120 },
+    { nom: 'PMDM', professor: 'Paco', hores: 100 },
+    { nom: 'PSP', professor: 'Roberto', hores: 60 },
+    { nom: 'SGE', professor: 'Belén', hores: 100 },
+    { nom: 'Anglés', professor: 'Caterina', hores: 40 },
+    { nom: 'EIE', professor: 'Ana', hores: 60 },
+  ];
 
   return (
     <PaperProvider>
       <View>
         {Nom("Unai Ruesta", { fontSize: 40, fontWeight: 'bold', margin: 20, color: 'black', textAlign: 'center' })}
         {Dades(['Email', 'Nom'], estil)}
-        {Informe()}
+        {isAdmin ? <Informe /> : null}
+
+        {moduls2Dam.map((elem, index) => (
+          <View
+            key={index}
+            style={[
+              { backgroundColor: index % 2 === 0 ? '#F48FB1' : '#F8BBD0', marginLeft: 10, marginRight: 10 },
+            ]}
+          >
+            <Text>{elem.nom}</Text>
+            <Text>Professor: {elem.professor}</Text>
+            <Text>Hores: {elem.hores}</Text>
+          </View>
+        ))}
+
       </View>
     </PaperProvider>
   );
@@ -61,6 +79,7 @@ const styles = {
     padding: 4,
     paddingLeft: 12,
     textAlign: 'left',
+    margin: 10,
   },
   florida: {
     backgroundColor: 'red',
@@ -69,7 +88,9 @@ const styles = {
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
+    margin: 10,
   },
+
 };
 
 export default Inici;
